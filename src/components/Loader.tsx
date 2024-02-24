0cb9674100026b2dfe182c6076b70bfadd508cd4
kkbejoy <kkbejoy@ymail.com>
2024-02-24 10:44:00 +0530

    lazy loading

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex  justify-center">
+      <h1 className="text-lg font-bold text-center">Loading</h1>
+    </div>
+  );
+};
+
+export default Loader;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import UserDetails from "../components/UserDetails";
-import ReloadButton from "../components/ReloadButton";
+import React, { Suspense, lazy } from "react";
+// import UserDetails from "../components/UserDetails";
+// import ReloadButton from "../components/ReloadButton";
+import Loader from "../components/Loader";
+//lazy loaded Componenets
+const UserDetails = lazy(() => import("../components/UserDetails"));
+const ReloadButton = lazy(() => import("../components/ReloadButton"));
+
 const HomePage: React.FC = () => {
   return (
     <div className="h-screen overflow-hidden ">
@@ -8,10 +13,14 @@ const HomePage: React.FC = () => {
       <div className="md:flex flex-col-3 justify-center  h-screen my-32 ">
         <div className="md:w-1/3"></div>
         <div className="flex md:w-1/3 justify-center ">
-          <UserDetails />{" "}
+          <Suspense fallback={<Loader />}>
+            <UserDetails />
+          </Suspense>
         </div>{" "}
         <div className="w-1/3 md:mt-44">
-          <ReloadButton />
+          <Suspense fallback={<Loader />}>
+            <ReloadButton />
+          </Suspense>
         </div>
       </div>
     </div>
