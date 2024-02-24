0efc190659547b4a7e1ad84fd1517bf360d48061
kkbejoy <kkbejoy@ymail.com>
2024-02-24 13:01:28 +0530

    strict mode disabled

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/ErrorBoundary/ErrorFallback.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ErrorBoundary fallback={<ErrorFallback />}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ErrorBoundary>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ErrorBoundary fallback={<ErrorFallback />}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ErrorBoundary>
+  // </React.StrictMode>
 );
