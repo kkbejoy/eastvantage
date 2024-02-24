30b94354b2962c30e9c0d8112b845a5f1f2befab
kkbejoy <kkbejoy@ymail.com>
2024-02-24 10:13:29 +0530

    responsiveness improved

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,8 +6,8 @@ const HomePage: React.FC = () => {
     <div className="h-screen overflow-hidden ">
       {/* <h1 className="text-center font-semibold">Eastvantage</h1> */}
       <div className="md:flex flex-col-3 justify-center  h-screen my-32 ">
-        <div className="w-1/3"></div>
-        <div className="flex w-1/3 justify-center ">
+        <div className="md:w-1/3"></div>
+        <div className="flex md:w-1/3 justify-center ">
           <UserDetails />{" "}
         </div>{" "}
         <div className="w-1/3 md:mt-44">
